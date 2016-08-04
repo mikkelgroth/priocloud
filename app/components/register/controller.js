222d15727fb4301328a8d4042e10967e156e3b7b
Mikkel Richter Damm <mrd@one.com>
2016-08-04 22:04:00 +0200

    register function implemented

--- app/components/register/controller.js
+++ app/components/register/controller.js
@@ -1,10 +1,61 @@
 angular
     .module('riskApp')
     .controller('RegisterController', [
-        '$scope', 
+        '$scope',
+        '$http',
+        'restService',
         function (
-            $scope
+            $scope,
+            $http,
+            restService
         ) {
 
+            $scope.register = function () {
+
+                $http
+                    .post(UERSERVER + '?action=createproject&application=priocloud&email=' + $scope.email + '&password=' + $scope.password)
+                    .success(function (data, status, headers, config) {
+
+                        if (data.authenticated) {
+
+                            restService
+                                .saveData('company', {
+                                    name: $scope.companyname,
+                                    projkpi1lable: 'Strategy',
+                                    projkpi2lable: 'Customer focus',
+                                    projkpi3lable: 'Compliance',
+                                    projkpi1weight: 1,
+                                    projkpi2weight: 1,
+                                    projkpi3weight: 1,
+                                    projkpi1sort: 'tophigh',
+                                    projkpi2sort: 'tophigh',
+                                    projkpi3sort: 'tophigh',
+                                    riskkpi1lable: 'Probability',
+                                    riskkpi2lable: 'Customer impact',
+                                    riskkpi3lable: 'Project delay',
+                                    riskkpi1weight: 1,
+                                    riskkpi2weight: 1,
+                                    riskkpi3weight: 1,
+                                    riskkpi1sort: 'tophigh',
+                                    riskkpi2sort: 'tophigh',
+                                    riskkpi3sort: 'tophigh'
+                                })
+                                .success(function (dataResponse) {
+
+                                    stateService.loadCompany();
+
+                                    restService
+                                        .saveData('bu', angular.fromJson({ name: $scope.companyname }))
+                                        .success(function (dataResponse) {
+                                            $scope.bus[0] = dataResponse;
+                                        });
+                                });
+
+                        } else {
+
+                            alert('User creation failure:\n\n' + data.message);
+                        }
+                    });
+            };
         }
     ]);
