5f5b6321660ba757ca9fbf3e217cd2ac29e46b91
Mikkel Richter Damm <mrd@one.com>
2016-08-19 14:49:42 +0200

    project status changed

--- app/components/projectsStatus/controller.js
+++ app/components/projectsStatus/controller.js
@@ -1,14 +1,51 @@
 angular
     .module('riskApp')
     .controller('ProjectsStatusController', [
-        '$scope', 
-        '$rootScope',
+        '$scope',
+        '$location',
+        'userService',
+        'companyService',
         function (
-            $scope, 
-            $rootScope
+            $scope,
+            $location,
+            userService,
+            companyService
         ) {
 
-            console.log("ProjectsStatusController init");
+            companyService.projects.subscribe(function (projects) {
 
+                console.log(projects);
+                $scope.projectList = setProjectList(projects);
+            });
+
+            companyService.businessUnits.subscribe(function (units) {
+
+                $scope.bus = units;
+            });
+
+            userService.users.subscribe(function (users) {
+
+                $scope.users = users;
+            });
+
+            $scope.goToProject = function (projectId) {
+
+		        $location.path('/project/' + projectId);
+            };
+
+            function setProjectList(projects) {
+
+                return projects.map(function (project) {
+
+                    project.buname = project.bu.name;
+                    project.poname = project.po.name;
+                    project.pmname = project.pm.name;
+
+                    // set last status
+                    project.lastStatus = project.statuses[project.statuses.length - 1];
+
+                    return project;
+                });
+            }
         }
     ]);
