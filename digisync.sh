6e0642ce42fdb6442476995ce7d53faef875ecc3
Theis Borg <theisborg@gmail.com>
2016-11-11 14:13:53 +0100

    removed old files

--- digisync.sh
+++ digisync.sh
@@ -1,4 +1,3 @@
-[ -f ./app/js/sitesettings.js ]  && { echo "Error: you have a settings file! I will not upload."; exit 2; }                           
-[ -f ./src/main/webapp/WEB-INF/config.properties ]  && { echo "Error: you have a settings file! I will not upload."; exit 2; }
+[ -f ./app/app.settings.js ]  && { echo "Error: you have a settings file! I will not upload."; exit 2; }                           
 rsync -zavr --no-perms app/ root@priocloud.com:/srv/www/dfds/app/
 rsync -zavr --no-perms app/ root@priocloud.com:/srv/www/priocloud/app/
