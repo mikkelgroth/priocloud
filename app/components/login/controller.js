773fabc4055a64575a170a85aabf0e8d7dddd2ac
Groth-Andersen Mikkel (DFDS A/S) <migro@dfds.com>
2017-01-26 20:04:07 +0100

    logout timer tyve min

--- app/components/login/controller.js
+++ app/components/login/controller.js
@@ -2,15 +2,49 @@ angular
     .module('riskApp')
     .controller('LoginController', [
         '$scope',
+        '$rootScope',
         '$http',
         '$location',
-        'userService',
+        'userService', 
+        '$timeout',
         function (
             $scope,
+            $rootScope,
             $http,
             $location,
-            userService
+            userService, 
+            $timeout
         ) {
+
+
+
+     //timeout functions
+  //displays a warning after 20 seconds, redirects to logout after another 10
+  
+  //$scope.inactive=false;
+  var idleTimer;  //warn the user about imminent timeout
+  var reloadTimer;  //redirect to logout page
+  function callIdle(){
+    console.log("You have been idle too long");
+    //$scope.inactive=true; //display GUI warning
+    reloadTimer=$timeout(callReload, 2*1000);
+  }
+  function callReload(){
+    console.log("Reload here...");
+    //$scope.inactive=false;
+    //$location.path("logout");
+      $rootScope.logout();
+  }
+  function movement(){
+    //console.log("movement: "+new Date());
+    $timeout.cancel(idleTimer);
+    $timeout.cancel(reloadTimer);
+    idleTimer=$timeout(callIdle, 20*60*1000);
+  }
+  angular.element(document.body).on('keydown DOMMouseScroll mousedown', movement);
+    
+
+
 
             $scope.login = function () {
 
@@ -23,19 +57,19 @@ angular
                             alert('Login failure:\n\n' + data.message);
 
                         } else {
-
+movement(); //init when login is done
                             userService.authenticate(data);
                             $location.path('/');
                         }
                     });
             };
-
+/*
             $scope.logout = function () {
-
+console.log("Logout...");
                 userService.invalidate();
                 $location.path('/');
             };
-
+*/
             $scope.resetPassword = function (email) {
 
                 $http
