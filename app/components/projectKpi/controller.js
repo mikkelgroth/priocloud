1712c86717edf3e616a4ea0dba97fd6968d109c4
Groth-Andersen Mikkel (DFDS A/S) <migro@dfds.com>
2017-11-19 15:49:42 +0100

    New radar chart in KPI Prototype

--- app/components/projectKpi/controller.js
+++ app/components/projectKpi/controller.js
@@ -62,10 +62,20 @@ angular
             };
                 
                 //radar stuff
-                 $scope.labels =[$scope.company.projkpi1lable, $scope.company.projkpi2lable, $scope.company.projkpi3lable, $scope.company.projkpi4lable, $scope.company.projkpi5lable, $scope.company.projkpi6lable];
+                $scope.radarkpilabels =[$scope.company.projkpi1lable, $scope.company.projkpi2lable, $scope.company.projkpi3lable, $scope.company.projkpi4lable, $scope.company.projkpi5lable, $scope.company.projkpi6lable];
+                $scope.radarkpidata = [[$scope.project.kpi1, $scope.project.kpi2, $scope.project.kpi3, $scope.project.kpi4, $scope.project.kpi5, $scope.project.kpi6]];
+                $scope.radarkpioptions = {
+                    responsive: false,
+                    maintainAspectRatio: false,
+                    scale: {
+                        ticks: {
+                            beginAtZero: true,
+                            max: 100
+                        }
+                    }
+                };
+                
 
-  $scope.data = [
-    [$scope.project.kpi1, $scope.project.kpi2, $scope.project.kpi3, $scope.project.kpi4, $scope.project.kpi5, $scope.project.kpi6]
-  ];
+
         }
     ]);
