48e5f2f99f47e2e5db7e2cf214a34356c7ef117d
Groth-Andersen Mikkel (DFDS A/S) <migro@dfds.com>
2018-02-08 18:39:46 +0100

    Dependencies version 2

--- app/components/projectsDependencies/controller.js
+++ app/components/projectsDependencies/controller.js
@@ -43,6 +43,73 @@
                 $location.path('/project/' + projectId + '/dependencies/' + depId);
             };
 
+            $scope.depbarRender = function (dep) {  
+                return depbarRender(dep);
+            }
+        
+            //depBarRender START
+            function depbarRender(dep) {
+                
+                                var s = new Date(Date.parse(dep.depdate));
+                                var e = new Date(Date.parse(dep.depdeaddate));
+                                var start = 0;
+                                var end = 100;
+                
+                                var thisyear = new Date();
+                                var base = new Date();
+                                var startbase = new Date();
+                                base.setFullYear(thisyear.getFullYear(), 0, 1);
+                                startbase.setFullYear(thisyear.getFullYear()-1, 0, 1);
+                
+                                var today = Math.round((((thisyear.getTime() - base.getTime()) / 86400000) * 33 / 365)+33);
+                                var oneday = today + 1;
+                
+                //console.log("thisyear-1 = " + (thisyear.getFullYear()-1));
+                
+                                if ((thisyear.getFullYear()-1) == s.getFullYear()||thisyear.getFullYear() == s.getFullYear()||(thisyear.getFullYear()+1) == s.getFullYear()) start = Math.round(((s.getTime() - startbase.getTime()) / 86400000) * 100 / (3*365));
+                                if ((thisyear.getFullYear()-1) == e.getFullYear()||thisyear.getFullYear() == e.getFullYear()||(thisyear.getFullYear()+1) == e.getFullYear()) end = Math.round(((e.getTime() - startbase.getTime()) / 86400000) * 100 / (3*365));
+                                if (start == end && end != 100) end = end + 1;
+                                if (start == 100) start--;
+                //console.log("Start = " + start);
+                //console.log("End = " + end);
+                
+                                var color = "grey";
+                                var colorlevel = "Blue";
+
+                                if (dep.agreement != null && dep.agreement != "") {
+                        
+                                    if (dep.agreement == "Conflict") colorlevel = "Red";
+                                    if (dep.agreement == "Tentative") colorlevel = "Yellow";
+                                    if (dep.agreement == "Agreed") colorlevel = "Green";
+                                }
+                
+                
+                                var ret = "#f6f1d3";
+                                if (start < end) {
+                                    if (today <= start) {
+                                        ret = "linear-gradient(to right, #f6f1d3 " + today + "%, " + color + " " + today + "%, " + color + " " + oneday + "%, #f6f1d3 " + oneday + "%, #f6f1d3 " + start + "%, " + colorlevel + " " + start + "%, " + colorlevel + " " + end + "%, #f6f1d3 " + end + "%)";
+                                    }
+                                    if (today > start && today <= end) {
+                                        ret = "linear-gradient(to right, #f6f1d3 " + start + "%, " + colorlevel + " " + start + "%, " + colorlevel + " " + today + "%, " + color + " " + today + "%, " + color + " " + oneday + "%, " + colorlevel + " " + oneday + "%," + colorlevel + " " + end + "%, #f6f1d3 " + end + "%)";
+                                    }
+                                    if (today > end) {
+                                        if (dep.state == "Open") color = "red";
+                                        ret = "linear-gradient(to right, #f6f1d3 " + start + "%, " + colorlevel + " " + start + "%, " + colorlevel + " " + end + "%, #f6f1d3 " + end + "%, #f6f1d3 " + today + "%, " + color + " " + today + "%, " + color + " " + oneday + "%, #f6f1d3 " + oneday + "%)";
+                                    }
+                                    if ((thisyear.getFullYear()-1) > s.getFullYear() && (thisyear.getFullYear()-1) > e.getFullYear()) {
+                                        var dist = Math.round(today / 4);
+                                        if (dep.state == "Open") color = "red";
+                                        ret = "linear-gradient(to right, " + colorlevel + " 0%, #f6f1d3 " + dist + "%, #f6f1d3 " + today + "%, " + color + " " + today + "%, " + color + " " + oneday + "%, #f6f1d3 " + oneday + "%)";
+                                    }
+                                    if ((thisyear.getFullYear()+1) < s.getFullYear() && (thisyear.getFullYear()+1) < e.getFullYear()) {
+                                        var dist = 100 - Math.round((100 - today) / 4);
+                                        ret = "linear-gradient(to right, #f6f1d3 " + today + "%, " + color + " " + today + "%, " + color + " " + oneday + "%, #f6f1d3 " + oneday + "%, #f6f1d3 " + dist + "%, " + colorlevel + " 100%)";
+                                    }
+                                }
+                
+                                return { background: ret }
+                            }
+                //depBarRender END
             
 
             function setDepList(projects) {
