cb86a7adea9f10cf8b7808222af1d4195cdc2b63
mga <mikkel.groth.privat@gmail.com>
2020-07-01 11:41:18 +0200

    delete DFDS from app.settings

--- app/app.settings.js
+++ app/app.settings.js
@@ -1,41 +1,11 @@
 var SITE = "Local";
-var SITENAME = "Local dev";
-DBSERVER = "https://priodev.northeurope.cloudapp.azure.com/priorest";
-USERSERVER = "https://priodev.northeurope.cloudapp.azure.com/priouser";
+var SITENAME = "Docker dev";
+    DBSERVER = "/priorest";
+    USERSERVER = "/priouser";
 var PRIVATECLOUD = "true";
 var SSO = "NOSSO";
 
-//Test env. 3 tier with Cosmos DB service
-if (window.location.href.indexOf("priocloudtest") > -1) {
-    SITENAME = "Test";
-    DBSERVER = "https://priocloudbackendtest.azurewebsites.net/mongorest/rest";
-    USERSERVER = "https://priocloudbackendtest.azurewebsites.net/mongorest/user";
-
-    //Dev with 2 tiers Frontend-Ubuntu with Backend and Mongo DB in dockers.  Or frontend docker in 1 tier   
-} else if (window.location.href.indexOf("prioclouddevfrontend") > -1) {
-    SITENAME = "Dev";
-    DBSERVER = "https://priodev.northeurope.cloudapp.azure.com/priorest";
-    USERSERVER = "https://priodev.northeurope.cloudapp.azure.com/priouser";
-    SSO = "SSO";
-
-    //localhost - docker running locally
-} else if (window.location.href.indexOf("localhost/") > -1 || window.location.href.indexOf("priocloud.com") > -1) {
-    SITENAME = "Docker dev";
-    DBSERVER = "/priorest";
-    USERSERVER = "/priouser";
-    SSO = "NOSSO";
-
-    //Dev No SSO 3 dockers on Ubuntu 
-} else if (window.location.href.indexOf("priodev.northeurope") > -1) {
-    SITENAME = "Docker%SSO";
-    DBSERVER = "https://priodev.northeurope.cloudapp.azure.com/priorest";
-    USERSERVER = "https://priodev.northeurope.cloudapp.azure.com/priouser";
-    SSO = "NOSSO";
-
-    //Prod with 2 tiers, Frontend-Ubuntu with Backend and Mongo DB in dockers. 
-} else if (window.location.href.indexOf("priocloud") > -1) {
+//
+if (window.location.href.indexOf("priocloud") > -1) {
     SITENAME = "PrioCloud 2.1";
-    DBSERVER = "https://priocloud.westeurope.cloudapp.azure.com/priorest";
-    USERSERVER = "https://priocloud.westeurope.cloudapp.azure.com/priouser";
-    SSO = "SSO";
 }
